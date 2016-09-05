1c30fb734bd12e93ebabc298837cf90c8e0a02fe
rmit-S3488937-Tyler-Larrad <s3488937@student.rmit.edu.au>
2016-09-05 12:02:13 +1000

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,21 @@
-public class Test extends Main {
+package SportsStore;
+// Test to see if Staff has "Password"
 
-	public static void main(String[] args) {
-		
+import org.junit.*;
 
-	}
+import SportsStore.Staff;
+
+public class Test {
+
+	Staff staff1 = new Staff("E12345", "Tyler", "password", "Manager");
+	Staff staff2 = new Staff("E54321", "Shahmeer", "", "Staff");
+	Staff staff3 = new Staff("E67890", "Patrick", "password", "Warehouse");
+	Staff staff4 = new Staff("E09876", "Chandi", "password", "Staff");
+
+	//@Before
+
+	//@Test
+
+	//@After
 
 }
